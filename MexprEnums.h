e4bb4d5bb9170f333af223cbb61bf53ef6003a26
beneslami <beneslami72@gmail.com>
2025-09-28 18:28:02 -0700

    update enums

--- MexprEnums.h
+++ MexprEnums.h
@@ -32,11 +32,13 @@ typedef enum {
     MATH_CPP_BRACKET_END,
     MATH_CPP_COMMA,
     MATH_CPP_OPR_INVALID,
+    MATH_CPP_SINGLE_QUOTED_STRING,
+    MATH_CPP_DOUBLE_QUOTED_STRING,
+    MATH_CPP_EMAIL,
     MATH_CPP_OPR_MAX,
-} mexprcpp_dtypes_t;
+} mexprcpp_operators_t;
 
 typedef enum {
-
     MATH_CPP_DTYPE_FIRST = (int) MATH_CPP_OPR_MAX + 1,
     MATH_CPP_INT ,
     MATH_CPP_DOUBLE,
@@ -45,14 +47,12 @@ typedef enum {
     MATH_CPP_BOOL,
     MATH_CPP_VARIABLE,
     MATH_CPP_DTYPE_LAST,
-
     MATH_CPP_DTYPE_WILDCRAD,
     MATH_CPP_DTYPE_INVALID,
     MATH_CPP_DTYPE_MAX
-    
 } mexprcpp_dtypes_t;
 
-const char *stringTypes[] = {
+/*const char *mexprcppOperatorsStringType[] = {
     "MATH_CPP_OPR_FIRST",
     "MATH_CPP_MOD",
     "MATH_CPP_PLUS",
@@ -77,5 +77,23 @@ const char *stringTypes[] = {
     "MATH_CPP_BRACKET_END",
     "MATH_CPP_COMMA",
     "MATH_CPP_OPR_INVALID",
+    "MATH_CPP_SINGLE_QUOTED_STRING",
+    "MATH_CPP_DOUBLE_QUOTED_STRING",
+    "MATH_CPP_EMAIL",
     "MATH_CPP_OPR_MAX",
 };
+
+const char *mexprcppDtypesStringType[] = {
+    "MATH_CPP_DTYPE_FIRST",
+    "MATH_CPP_INT",
+    "MATH_CPP_DOUBLE",
+    "MATH_CPP_STRING",
+    "MATH_CPP_IPV4",
+    "MATH_CPP_BOOL",
+    "MATH_CPP_VARIABLE",
+    "MATH_CPP_DTYPE_LAST",
+    "MATH_CPP_DTYPE_WILDCRAD",
+    "MATH_CPP_DTYPE_INVALID",
+    "MATH_CPP_DTYPE_MAX",
+};
+*/
